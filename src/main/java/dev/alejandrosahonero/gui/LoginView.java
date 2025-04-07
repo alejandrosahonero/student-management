5edd281f1cf34c4c1bfa5d946d33f20ec4585beb
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-07 18:05:30 +0200

    Add better login button design

--- src/main/java/dev/alejandrosahonero/gui/LoginView.java
+++ src/main/java/dev/alejandrosahonero/gui/LoginView.java
@@ -8,22 +8,19 @@ public class LoginView extends JFrame {
     final int windowWidth=1152;
     final int windowHeight=768;
     //Tamaño del boton
-    final int loginButtonWidth=150;
-    final int loginButtonHeight=70;
-    //Tamaño del contenedor del boton
-    final int loginPanelWidth=0;
-    final int loginPanelHeight=110;
+    final int loginButtonWidth=180;
+    final int loginButtonHeight=90;
 
+    JLayeredPane loginPanel;
     ImageIcon icon, background;
-    JPanel loginPanel;
-    JButton login;
+    JButton loginButton;
 
     public LoginView() {
         setTitle("CO-Gestor");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setSize(windowWidth, windowHeight);
-        setLayout(new BorderLayout());
+        setLayout(null);
         setBackground(new Color(0xCCCCCC));
         initComponents();
     }
@@ -32,6 +29,8 @@ public class LoginView extends JFrame {
      * Método que inicializa todos los demás métodos para aligerar la carga al constructor
      */
     public void initComponents() {
+        loginPanel = getLayeredPane();
+        loginPanel.setBounds(0, 0, windowWidth, windowHeight);
         WindowIcon();
         BackgroundImage();
         loginButton();
@@ -50,11 +49,10 @@ public class LoginView extends JFrame {
      */
     public void BackgroundImage() {
         background = new ImageIcon("src/main/resources/img/background-comercio.png");
-        background = new ImageIcon(background.getImage().getScaledInstance(windowWidth, windowHeight-loginButtonHeight-loginButtonHeight, Image.SCALE_SMOOTH));
-
         JLabel backgroundLabel = new JLabel(background);
 
-        add(backgroundLabel, BorderLayout.CENTER);
+        backgroundLabel.setBounds(0, 0, windowWidth, windowHeight);
+        loginPanel.add(backgroundLabel, 1);
     }
 
     /**
@@ -62,18 +60,21 @@ public class LoginView extends JFrame {
      * Añade el boton a un panel para poder alinearlo correctamente y a la vez tener un tamaño especificado
      */
     public void loginButton(){
-        login = new JButton();
-        login.setFont(new Font("Arial", Font.BOLD, 20));
-        login.setPreferredSize(new Dimension(loginButtonWidth, loginButtonHeight));
+        loginButton = new JButton();
+        loginButton.setFont(new Font("Arial", Font.BOLD, 20));
+        loginButton.setBorder(BorderFactory.createLineBorder(new Color(0x3c7181),10,true));
+        //loginButton.setBackground(null);
 
         ImageIcon buttonIcon = new ImageIcon("src/main/resources/img/login-button.png");
         buttonIcon = new ImageIcon(buttonIcon.getImage().getScaledInstance(loginButtonWidth, loginButtonHeight, Image.SCALE_SMOOTH));
-        login.setIcon(buttonIcon);
+        loginButton.setIcon(buttonIcon);
 
-        loginPanel = new JPanel(new GridBagLayout());
-        loginPanel.setPreferredSize(new Dimension(loginPanelWidth, loginPanelHeight));
-        loginPanel.add(login);
+        loginButton.setBounds((windowWidth/2)-(loginButtonWidth/2), windowHeight-250, loginButtonWidth, loginButtonHeight);
+        loginPanel.add(loginButton,2);
 
-        add(loginPanel, BorderLayout.SOUTH);
+    }
+
+    public JButton getLoginButton() {
+        return loginButton;
     }
 }

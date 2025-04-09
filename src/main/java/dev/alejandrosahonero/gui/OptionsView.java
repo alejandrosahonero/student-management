b29062d7f431920d0d6806ab2054d1404b6d6c7a
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-09 21:28:27 +0200

    Add a window general background and fix buttons positions of OptionsView

--- src/main/java/dev/alejandrosahonero/gui/OptionsView.java
+++ src/main/java/dev/alejandrosahonero/gui/OptionsView.java
@@ -6,30 +6,42 @@ import javax.swing.*;
 import java.awt.*;
 
 public class OptionsView extends JFrame {
-    private JPanel panel;
+    private JPanel globalPanel, panel;
     private JButton matricularButton, listarButton, addProButton, addModButton;
+    private ImageIcon background;
     public OptionsView(){
         setTitle("Opciones");
         setSize(new Dimension(1152, 768));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
-        setLayout(new BorderLayout());
+        setLayout(null);
 
         initComponents();
     }
     private void initComponents(){
+        backgroundImage();
         buttonsPanel();
     }
+    private void backgroundImage() {
+        background = new ImageIcon("src/main/resources/img/background-windows.png");
+        background = new ImageIcon(background.getImage().getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_SMOOTH));
+
+        JLabel backgroundLabel = new JLabel(background);
+        backgroundLabel.setBounds(0, 0, this.getWidth(), this.getHeight());
+
+        add(backgroundLabel);
+    }
     private void buttonsPanel(){
-        panel = new JPanel(new GridLayout(2,2));
+        final int buttonWidth = 250;
+        final int buttonHeight = 100;
 
         matricularButton = new JButton("Matricular");
         matricularButton.setFont(new Font("Tahoma", Font.BOLD, 20));
         matricularButton.setHorizontalAlignment(SwingConstants.CENTER);
         matricularButton.setVerticalAlignment(SwingConstants.CENTER);
-        matricularButton.setPreferredSize(new Dimension(250, 100));
         matricularButton.addActionListener(new OptionsController(this));
+        matricularButton.setBounds((getWidth()/2-buttonWidth/2)/2, (getHeight()/2-buttonHeight/2)/2, buttonWidth, buttonHeight);
 
         listarButton = new JButton("Listar");
         listarButton.setFont(new Font("Tahoma", Font.BOLD, 20));
@@ -37,6 +49,7 @@ public class OptionsView extends JFrame {
         listarButton.setVerticalAlignment(SwingConstants.CENTER);
         listarButton.setPreferredSize(new Dimension(250, 100));
         listarButton.addActionListener(new OptionsController(this));
+        listarButton.setBounds((getWidth()/2+buttonWidth/2), (getHeight()/2-buttonHeight/2)/2, buttonWidth, buttonHeight);
 
         addProButton = new JButton("Añadir Profesor");
         addProButton.setFont(new Font("Tahoma", Font.BOLD, 20));
@@ -44,6 +57,7 @@ public class OptionsView extends JFrame {
         addProButton.setVerticalAlignment(SwingConstants.CENTER);
         addProButton.setPreferredSize(new Dimension(250, 100));
         addProButton.addActionListener(new OptionsController(this));
+        addProButton.setBounds((getWidth()/2-buttonWidth/2)/2, (getHeight()/2+buttonHeight/2), buttonWidth, buttonHeight);
 
         addModButton = new JButton("Añadir módulo");
         addModButton.setFont(new Font("Tahoma", Font.BOLD, 20));
@@ -51,22 +65,12 @@ public class OptionsView extends JFrame {
         addModButton.setVerticalAlignment(SwingConstants.CENTER);
         addModButton.setPreferredSize(new Dimension(250, 100));
         addModButton.addActionListener(new OptionsController(this));
+        addModButton.setBounds((getWidth()/2+buttonWidth/2), (getHeight()/2+buttonHeight/2), buttonWidth, buttonHeight);
 
-        JPanel button1 = new JPanel(new GridBagLayout());
-        button1.add(matricularButton);
-        JPanel button2 = new JPanel(new GridBagLayout());
-        button2.add(listarButton);
-        JPanel button3 = new JPanel(new GridBagLayout());
-        button3.add(addProButton);
-        JPanel button4 = new JPanel(new GridBagLayout());
-        button4.add(addModButton);
-
-        panel.add(button1);
-        panel.add(button2);
-        panel.add(button3);
-        panel.add(button4);
-
-        add(panel);
+        add(matricularButton);
+        add(listarButton);
+        add(addProButton);
+        add(addModButton);
     }
 
     public JButton getMatricularButton() {

f749fde0043b2077c9a57d5d1a2d31eb3f60c8f2
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-22 19:47:16 +0200

    Add buttons colors

--- src/main/java/dev/alejandrosahonero/gui/OptionsView.java
+++ src/main/java/dev/alejandrosahonero/gui/OptionsView.java
@@ -26,30 +26,42 @@ public class OptionsView extends MainView {
         final int buttonHeight = 100;
 
         matricularButton = new JButton("Matricular");
-        matricularButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        matricularButton.setFont(new Font("Tahoma", Font.BOLD, 30));
         matricularButton.setHorizontalAlignment(SwingConstants.CENTER);
         matricularButton.setVerticalAlignment(SwingConstants.CENTER);
+        matricularButton.setBackground(new Color(0x3D5A40));
+        matricularButton.setForeground(new Color(0xffffff));
+        matricularButton.setFocusable(false);
         matricularButton.addActionListener(new OptionsController(this));
         matricularButton.setBounds((getWidth()/2-buttonWidth/2)/2, (getHeight()/2-buttonHeight/2)/2, buttonWidth, buttonHeight);
 
         listarButton = new JButton("Listar");
-        listarButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        listarButton.setFont(new Font("Tahoma", Font.BOLD, 30));
         listarButton.setHorizontalAlignment(SwingConstants.CENTER);
         listarButton.setVerticalAlignment(SwingConstants.CENTER);
+        listarButton.setBackground(new Color(0x3D5A40));
+        listarButton.setForeground(new Color(0xffffff));
+        listarButton.setFocusable(false);
         listarButton.addActionListener(new OptionsController(this));
         listarButton.setBounds((getWidth()/2+buttonWidth/2), (getHeight()/2-buttonHeight/2)/2, buttonWidth, buttonHeight);
 
         addProButton = new JButton("Añadir Profesor");
-        addProButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        addProButton.setFont(new Font("Tahoma", Font.BOLD, 30));
         addProButton.setHorizontalAlignment(SwingConstants.CENTER);
         addProButton.setVerticalAlignment(SwingConstants.CENTER);
+        addProButton.setBackground(new Color(0x3D5A40));
+        addProButton.setForeground(new Color(0xffffff));
+        addProButton.setFocusable(false);
         addProButton.addActionListener(new OptionsController(this));
         addProButton.setBounds((getWidth()/2-buttonWidth/2)/2, (getHeight()/2+buttonHeight/2), buttonWidth, buttonHeight);
 
         addModButton = new JButton("Añadir módulo");
-        addModButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+        addModButton.setFont(new Font("Tahoma", Font.BOLD, 30));
         addModButton.setHorizontalAlignment(SwingConstants.CENTER);
         addModButton.setVerticalAlignment(SwingConstants.CENTER);
+        addModButton.setBackground(new Color(0x3D5A40));
+        addModButton.setForeground(new Color(0xffffff));
+        addModButton.setFocusable(false);
         addModButton.addActionListener(new OptionsController(this));
         addModButton.setBounds((getWidth()/2+buttonWidth/2), (getHeight()/2+buttonHeight/2), buttonWidth, buttonHeight);
 

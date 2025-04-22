eb37c4809180894bad3b00da209e1df35bd0bb43
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-22 18:42:04 +0200

    Add new settings button, better and responsive design

--- src/main/java/dev/alejandrosahonero/gui/StartView.java
+++ src/main/java/dev/alejandrosahonero/gui/StartView.java
@@ -8,9 +8,10 @@ import java.awt.*;
 
 public class StartView extends MainView {
     final int buttonPanelHeight = 80;
+    final int buttonsWidth = 260;
 
     private JLabel background;
-    private JButton loginButton, registerButton;
+    private JButton loginButton, registerButton, settingsButton;
     private JPanel buttonPanel;
 
     public StartView() {
@@ -24,16 +25,22 @@ public class StartView extends MainView {
      * Método que inicializa todos los demás métodos para aligerar la carga al constructor
      */
     private void initComponents() {
-        ImageIcon newBackground = new ImageIcon(getBackgroundImage().getImage().getScaledInstance(getWidth(),getHeight()-buttonPanelHeight, Image.SCALE_SMOOTH));
-        background = new JLabel(newBackground);
-        buttonPanel = new JPanel(new GridBagLayout());
-        buttonPanel.setPreferredSize(new Dimension(getWidth(),buttonPanelHeight));
-
+        view();
         registerButton();
         loginButton();
+        settingsButton();
 
         add(background, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    private void view(){
+        ImageIcon newBackground = new ImageIcon(getBackgroundImage().getImage().getScaledInstance(getWidth(),getHeight()-buttonPanelHeight, Image.SCALE_SMOOTH));
+        background = new JLabel(newBackground);
+
+        buttonPanel = new JPanel(new BorderLayout(10,10));
+        buttonPanel.setPreferredSize(new Dimension(getWidth(),buttonPanelHeight));
+        buttonPanel.setBackground(new Color(0x888d81));
     }
 
 
@@ -41,35 +48,45 @@ public class StartView extends MainView {
      * Crea el botón de inicio
      */
     private void loginButton() {
-        int thiswidth = 260;
-        int thisheight = 60;
-
         loginButton = new JButton("INICIAR SESION");
         loginButton.addActionListener(new LoginButtonController(this));
 
-        loginButton.setPreferredSize(new Dimension(thiswidth,thisheight));
-        loginButton.setFont(new Font("Arial", Font.BOLD, 28));
+        loginButton.setPreferredSize(new Dimension(buttonsWidth,0));
+        loginButton.setFont(new Font("Arial", Font.BOLD, 25));
         loginButton.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         loginButton.setBackground(new Color(0x2C3E50));
         loginButton.setForeground(new Color(0xffffff));
         loginButton.setFocusPainted(false);
+        loginButton.setVerticalAlignment(SwingConstants.CENTER);
 
-        buttonPanel.add(loginButton);
+        buttonPanel.add(loginButton, BorderLayout.CENTER);
     }
     private void registerButton() {
-        int thiswidth = 260;
-        int thisheight = 60;
-
         registerButton = new JButton("REGISTRARSE");
         registerButton.addActionListener(new RegisterButtonController(this));
 
-        registerButton.setPreferredSize(new Dimension(thiswidth,thisheight));
-        registerButton.setFont(new Font("Arial", Font.BOLD, 14));
+        registerButton.setPreferredSize(new Dimension(buttonsWidth,0));
+        registerButton.setFont(new Font("Arial", Font.BOLD, 25));
         registerButton.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         registerButton.setBackground(new Color(0x5A4E44));
         registerButton.setForeground(new Color(0xffffff));
         registerButton.setFocusPainted(false);
+        registerButton.setVerticalAlignment(SwingConstants.CENTER);
 
-        buttonPanel.add(registerButton);
+        buttonPanel.add(registerButton , BorderLayout.EAST);
+    }
+    private void settingsButton() {
+        settingsButton = new JButton("AJUSTES");
+        settingsButton.addActionListener(new RegisterButtonController(this));
+
+        settingsButton.setPreferredSize(new Dimension(buttonsWidth,0));
+        settingsButton.setFont(new Font("Arial", Font.BOLD, 25));
+        settingsButton.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        settingsButton.setBackground(new Color(0x5A4E44));
+        settingsButton.setForeground(new Color(0xffffff));
+        settingsButton.setFocusPainted(false);
+        settingsButton.setVerticalAlignment(SwingConstants.CENTER);
+
+        buttonPanel.add(settingsButton , BorderLayout.WEST);
     }
 }

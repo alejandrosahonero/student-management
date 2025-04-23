2807e1703044ab1b86056ed93ad6d8a94021930b
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-23 21:39:10 +0200

    Add connection to database and update alum table info

--- src/main/java/dev/alejandrosahonero/gui/options/list/ListAlumView.java
+++ src/main/java/dev/alejandrosahonero/gui/options/list/ListAlumView.java
@@ -1,14 +1,18 @@
 package dev.alejandrosahonero.gui.options.list;
 
 import dev.alejandrosahonero.gui.MainView;
+import dev.alejandrosahonero.logic.options.list.ListAlumController;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class ListAlumView extends MainView {
     private JLabel background;
+    private DefaultTableModel model;
     private JTable listAlum;
     private JPanel tablePanel;
+    private ListAlumController listAlumController;
 
     public ListAlumView() {
         super("Alumnos");
@@ -27,25 +31,19 @@ public class ListAlumView extends MainView {
         tablePanel = new JPanel(new BorderLayout());
         tablePanel.setBounds(getWindowsWidth()/30, getWindowsHeight()/20, getWindowsWidth()-getWindowsWidth()/25*2, getWindowsHeight()-getWindowsHeight()/3);
 
-        String[] columnNames = {"ID Matrícula", "Apellido Paterno", "Apellido Materno", "Nombres", "Edad"};
-        Object[][] data = {
-                {"1000", "Smith", "Smith",
-                        "Juan", 5},
-                {"1001", "Doe", "Smith",
-                        "Pepe", 3},
-                {"1002", "Black", "Smith",
-                        "Victor", 2},
-                {"1003", "White", "Smith",
-                        "Andres Vicent", 20},
-                {"1004", "Brown", "Smith",
-                        "Paul", 10}
-        };
-
-        listAlum = new JTable(data, columnNames);
+        model = new DefaultTableModel(new String[]{"ID Matrícula", "Apellido Paterno", "Apellido Materno", "Nombres", "Edad"}, 0);
+        listAlum = new JTable(model);
 
         JScrollPane scrollPane = new JScrollPane(listAlum);
         tablePanel.add(scrollPane);
 
+        listAlumController = new ListAlumController(this);
+        listAlumController.cargarDatos(model);
+
         background.add(tablePanel);
+    }
+
+    public JTable getListAlum() {
+        return listAlum;
     }
 }
--- src/main/java/dev/alejandrosahonero/logic/options/list/ListAlumController.java
+++ src/main/java/dev/alejandrosahonero/logic/options/list/ListAlumController.java
@@ -1,4 +1,35 @@
 package dev.alejandrosahonero.logic.options.list;
 
+import dev.alejandrosahonero.db.Alumno;
+import dev.alejandrosahonero.db.Conector;
+import dev.alejandrosahonero.gui.options.list.ListAlumView;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+
 public class ListAlumController {
+    private ListAlumView listAlumView;
+
+    public ListAlumController(ListAlumView listAlumView){
+        this.listAlumView = listAlumView;
+    }
+    public void cargarDatos(DefaultTableModel model){
+        EntityManager em = Conector.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            TypedQuery<Alumno> query = em.createQuery("SELECT a FROM Alumno a ORDER BY a.IDMatricula", Alumno.class);
+            List<Alumno> lista = query.getResultList();
+            for (Alumno a : lista) {
+                model.addRow(new Object[]{a.getIDMatricula(), a.getApellidoPaterno(), a.getApellidoMaterno(), a.getNombres(), a.getEdad()});
+            }
+            em.getTransaction().commit();
+            em.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(listAlumView, "Error al cargar datos: " + e.getMessage());
+            em.close();
+        }
+    }
 }

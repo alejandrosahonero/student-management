907ca53da7fddb8a661e26fa14f56e46d3e2afc5
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-23 21:02:35 +0200

    improve structure

--- src/main/java/dev/alejandrosahonero/db/Persona.java
+++ src/main/java/dev/alejandrosahonero/db/Persona.java
@@ -28,39 +28,30 @@ public class Persona implements Serializable {
     public String getDni() {
         return dni;
     }
-
     public void setDni(String dni) {
         this.dni = dni;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public String getApellido() {
         return apellido;
     }
-
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-
     public int getEdad() {
         return edad;
     }
-
     public void setEdad(int edad) {
         this.edad = edad;
     }
-
     public String getSexo() {
         return sexo;
     }
-
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }

5df5d1b240fe9a3e992ac2efdaec8abd88da064b
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-23 20:54:56 +0200

    Add new attribute

--- src/main/java/dev/alejandrosahonero/db/Alumno.java
+++ src/main/java/dev/alejandrosahonero/db/Alumno.java
@@ -1,15 +1,19 @@
 package dev.alejandrosahonero.db;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 @Entity
 public class Alumno extends Persona implements Serializable {
+    private int IDMatricula;
     private ArrayList<String> modulosCursa = new ArrayList<String>();
+
     public Alumno() {}
-    public Alumno(String dni, String apellido, String nombre, int edad, String sexo, ArrayList<String> modulosCursa) {
+    public Alumno(String dni, String apellido, String nombre, int edad, String sexo, int IDMatricula, ArrayList<String> modulosCursa) {
         super(dni, apellido, nombre, edad, sexo);
+        this.IDMatricula = IDMatricula;
         this.modulosCursa = modulosCursa;
     }
 }

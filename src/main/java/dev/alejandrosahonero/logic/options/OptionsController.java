901b7397c5f76f02c24a55ba893b99d9ade880ae
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-23 19:16:10 +0200

    Add all windows actions

--- src/main/java/dev/alejandrosahonero/logic/options/OptionsController.java
+++ src/main/java/dev/alejandrosahonero/logic/options/OptionsController.java
@@ -1,6 +1,11 @@
 package dev.alejandrosahonero.logic.options;
 
 import dev.alejandrosahonero.gui.options.*;
+import dev.alejandrosahonero.gui.options.list.ListAlumView;
+import dev.alejandrosahonero.gui.options.list.ListAsigView;
+import dev.alejandrosahonero.gui.options.list.ListModView;
+import dev.alejandrosahonero.gui.options.list.ListProView;
+import dev.alejandrosahonero.gui.options.manage.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,21 +17,41 @@ public class OptionsController implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
-        /*if(e.getSource() == optionsView.getMatricularButton()){
+        if(e.getSource() == optionsView.getMatricularButton()){
             optionsView.setVisible(false);
             new MatricularView().setVisible(true);
         }
-        if(e.getSource() == optionsView.getListarButton()){
+        if(e.getSource() == optionsView.getModAlumButton()){
             optionsView.setVisible(false);
-            new ListarView().setVisible(true);
+            new ModAlumView().setVisible(true);
         }
-        if(e.getSource() == optionsView.getAddProButton()){
+        if(e.getSource() == optionsView.getModProButton()){
             optionsView.setVisible(false);
             new ModProView().setVisible(true);
         }
-        if(e.getSource() == optionsView.getAddModButton()){
+        if(e.getSource() == optionsView.getModModButton()){
             optionsView.setVisible(false);
             new ModModView().setVisible(true);
-        }*/
+        }
+        if(e.getSource() == optionsView.getModAsigButton()){
+            optionsView.setVisible(false);
+            new ModAsigView().setVisible(true);
+        }
+        if(e.getSource() == optionsView.getListAlumButton()){
+            optionsView.setVisible(false);
+            new ListAlumView().setVisible(true);
+        }
+        if(e.getSource() == optionsView.getListProButton()){
+            optionsView.setVisible(false);
+            new ListProView().setVisible(true);
+        }
+        if(e.getSource() == optionsView.getListModButton()){
+            optionsView.setVisible(false);
+            new ListModView().setVisible(true);
+        }
+        if(e.getSource() == optionsView.getListAsigButton()){
+            optionsView.setVisible(false);
+            new ListAsigView().setVisible(true);
+        }
     }
 }

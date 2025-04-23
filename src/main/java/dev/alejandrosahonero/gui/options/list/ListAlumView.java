efd68b3213a239202af293c88491df0952ea93e2
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-23 19:45:36 +0200

    Add example table

--- src/main/java/dev/alejandrosahonero/gui/options/list/ListAlumView.java
+++ src/main/java/dev/alejandrosahonero/gui/options/list/ListAlumView.java
@@ -3,17 +3,51 @@ package dev.alejandrosahonero.gui.options.list;
 import dev.alejandrosahonero.gui.MainView;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ListAlumView extends MainView {
     private JLabel background;
+    private JTable listAlum;
+    private JPanel tablePanel;
 
     public ListAlumView() {
         super("Modificar Alumno");
-
         initComponents();
     }
     private void initComponents(){
         background = new JLabel(getBackgroundImage());
+
+        table();
+
         add(background);
+    }
+    private void table(){
+        tablePanel = new JPanel(new BorderLayout());
+        tablePanel.setBounds(100, 100, 800, 400);
+
+        String[] columnNames = {"First Name",
+                "Last Name",
+                "Sport",
+                "# of Years",
+                "Vegetarian"};
+        Object[][] data = {
+                {"Kathy", "Smith",
+                        "Snowboarding", 5, false},
+                {"John", "Doe",
+                        "Rowing", 3, true},
+                {"Sue", "Black",
+                        "Knitting", 2, false},
+                {"Jane", "White",
+                        "Speed reading", 20, true},
+                {"Joe", "Brown",
+                        "Pool", 10, false}
+        };
+
+        listAlum = new JTable(data, columnNames);
+
+        JScrollPane scrollPane = new JScrollPane(listAlum);
+        tablePanel.add(scrollPane);
+
+        background.add(tablePanel);
     }
 }

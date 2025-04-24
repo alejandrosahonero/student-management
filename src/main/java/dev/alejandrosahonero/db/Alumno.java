71d33d42de04f0b4c5ae3207cefefc8fc5e6e94b
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-24 19:46:35 +0200

    Add nuss var and change arraylist type

--- src/main/java/dev/alejandrosahonero/db/Alumno.java
+++ src/main/java/dev/alejandrosahonero/db/Alumno.java
@@ -1,26 +1,37 @@
 package dev.alejandrosahonero.db;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 @Entity
 public class Alumno extends Persona implements Serializable {
+    @GeneratedValue
     private int id;
-    private ArrayList<Modulo> modulosCursa = new ArrayList<>();
+    private String modulo;
+    private String nuss;
+    private ArrayList<Asignatura> asignaturas = new ArrayList<>();
 
     public Alumno() {}
-    public Alumno(String dni, String apellidoPaterno, String apellidoMaterno, String nombre, short edad, String sexo, int id, ArrayList<Modulo> modulosCursa) {
+    public Alumno(String dni, String apellidoPaterno, String apellidoMaterno, String nombre, short edad, String sexo, String modulo, String nuss, ArrayList<Asignatura> asignaturas) {
         super(dni, apellidoPaterno, apellidoMaterno, nombre, edad, sexo);
-        this.id = id;
-        this.modulosCursa = modulosCursa;
+        this.modulo = modulo;
+        this.nuss = nuss;
+        this.asignaturas = asignaturas;
     }
 
     //Getters
     public int getId() {
         return id;
     }
-    public ArrayList<Modulo> getModulosCursa() {
-        return modulosCursa;
+    public String getModulo() {
+        return modulo;
+    }
+    public String getNuss() {
+        return nuss;
+    }
+    public ArrayList<Asignatura> getAsignaturas() {
+        return asignaturas;
     }
 }

7a256b1b53247820419d19c911924dd6232489d6
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-25 19:24:19 +0200

    Change constructor because now id is GeneratedValue

--- src/main/java/dev/alejandrosahonero/db/Profesor.java
+++ src/main/java/dev/alejandrosahonero/db/Profesor.java
@@ -1,18 +1,19 @@
 package dev.alejandrosahonero.db;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 @Entity
 public class Profesor extends Persona implements Serializable {
+    @GeneratedValue
     private int id;
     private ArrayList<Modulo> modulosImparte = new ArrayList<>();
 
     public Profesor() {}
-    public Profesor(String dni, String apellidoPaterno, String apellidoMaterno, String nombre, short edad, String sexo, int id, ArrayList<Modulo> modulosImparte) {
+    public Profesor(String dni, String apellidoPaterno, String apellidoMaterno, String nombre, short edad, String sexo, ArrayList<Modulo> modulosImparte) {
         super(dni, apellidoPaterno, apellidoMaterno, nombre, edad, sexo);
-        this.id = id;
         this.modulosImparte = modulosImparte;
     }
 

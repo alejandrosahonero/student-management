c47efc542a74491483939746072ffc93c429010d
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-25 19:54:15 +0200

    Improve example database

--- src/main/java/dev/alejandrosahonero/app/Main.java
+++ src/main/java/dev/alejandrosahonero/app/Main.java
@@ -3,15 +3,13 @@ package dev.alejandrosahonero.app;
 import dev.alejandrosahonero.db.*;
 import dev.alejandrosahonero.gui.welcome.StartView;
 
-import javax.persistence.EntityManager;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         StartView startView = new StartView();
         startView.setVisible(true);
 
-        ExampleDB.create();
+        //Example database (use only if dont have "database.odb" file)
+        //ExampleDB.create();
     }
 }

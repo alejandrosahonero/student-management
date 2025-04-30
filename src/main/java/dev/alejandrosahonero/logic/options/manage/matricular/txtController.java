49b9a535f009b8c6aa262da1e5f12d7f0aff026b
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-30 16:41:31 +0200

    Change "implements" to "extends" for better performance

--- src/main/java/dev/alejandrosahonero/logic/options/manage/matricular/txtController.java
+++ src/main/java/dev/alejandrosahonero/logic/options/manage/matricular/txtController.java
@@ -3,27 +3,24 @@ package dev.alejandrosahonero.logic.options.manage.matricular;
 import dev.alejandrosahonero.gui.options.manage.MatricularView;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class txtController implements MouseListener
-{
+public class txtController extends MouseAdapter {
     MatricularView matricularView;
 
     public txtController(MatricularView matricularView)
     {
         this.matricularView = matricularView;
     }
-    public void mouseClicked(MouseEvent e)
-    {
+    public void mouseClicked(MouseEvent e) {
         if(((JTextField)e.getSource()).getBackground() == Color.WHITE) {
             ((JTextField) e.getSource()).setText("");
             ((JTextField) e.getSource()).setBackground(new Color(255, 255, 254));
         }
     }
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
-    public void mousePressed(MouseEvent e) {}
-    public void mouseReleased(MouseEvent e) {}
 }

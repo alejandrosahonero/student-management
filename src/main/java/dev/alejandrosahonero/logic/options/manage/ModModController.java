1e566de0fc041eeedd4ba85c637a039b995ae1cd
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-04-30 17:09:29 +0200

    Add getter of modulos list connected to database

--- src/main/java/dev/alejandrosahonero/logic/options/manage/ModModController.java
+++ src/main/java/dev/alejandrosahonero/logic/options/manage/ModModController.java
@@ -1,4 +1,20 @@
 package dev.alejandrosahonero.logic.options.manage;
 
+import dev.alejandrosahonero.db.Conector;
+import dev.alejandrosahonero.db.Modulo;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
 public class ModModController {
+    public static List<String> getAllModulos(){
+        EntityManager em = Conector.getEntityManager();
+        em.getTransaction().begin();
+        Query query = em.createQuery("SELECT m.siglas FROM Modulo m");
+        List<String> allModulos = query.getResultList();
+        em.getTransaction().commit();
+        em.close();
+        return allModulos;
+    }
 }

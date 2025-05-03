237c18e1731ad033cf3627bb14d51b21b4c93973
alejandrodaniel <alesaho05@gmail.com>
2025-05-03 20:28:02 +0200

    Delete imports

--- src/main/java/dev/alejandrosahonero/gui/options/manage/MatricularView.java
+++ src/main/java/dev/alejandrosahonero/gui/options/manage/MatricularView.java
@@ -1,16 +1,5 @@
 package dev.alejandrosahonero.gui.options.manage;
 
-import dev.alejandrosahonero.gui.MainView;
-import dev.alejandrosahonero.logic.options.manage.ModModController;
-import dev.alejandrosahonero.logic.options.manage.matricular.MatricularButtonController;
-import dev.alejandrosahonero.logic.options.manage.PhotoButtonController;
-import dev.alejandrosahonero.logic.options.manage.matricular.txtController;
-
-import javax.swing.*;
-import java.awt.*;
-import java.util.List;
-import java.util.Objects;
-
 public class MatricularView extends MainManageView
 {
     public MatricularView(){

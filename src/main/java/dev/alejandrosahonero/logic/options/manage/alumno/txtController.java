9ce647ec42be5d0d0f4c87526ab077a60b22d2d5
alejandrodaniel <alesaho05@gmail.com>
2025-05-03 20:28:31 +0200

    Delete imports

--- src/main/java/dev/alejandrosahonero/logic/options/manage/alumno/txtController.java
+++ src/main/java/dev/alejandrosahonero/logic/options/manage/alumno/txtController.java
@@ -1,14 +1,13 @@
-package dev.alejandrosahonero.logic.options.manage.matricular;
+package dev.alejandrosahonero.logic.options.manage.alumno;
 
 import dev.alejandrosahonero.gui.options.manage.MatricularView;
 
 import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class txtController extends MouseAdapter {
     MatricularView matricularView;

c490e285a38a477684c16f4d370abfa8a3dc15ac
alejandrodaniel <alesaho05@gmail.com>
2025-05-03 21:04:47 +0200

    Controller now get data of Alum with searchbar

---
+++ src/main/java/dev/alejandrosahonero/logic/options/manage/alumno/ModAlumController.java
@@ -0,0 +1,43 @@
+package dev.alejandrosahonero.logic.options.manage.alumno;
+
+import dev.alejandrosahonero.db.Alumno;
+import dev.alejandrosahonero.db.Conector;
+import dev.alejandrosahonero.gui.options.manage.ModAlumView;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ModAlumController implements ActionListener {
+    private ModAlumView modAlumView;
+    public ModAlumController(ModAlumView modAlumView) {
+        this.modAlumView = modAlumView;
+    }
+    public void actionPerformed(ActionEvent e) {
+        //Set JTextFields texts
+        try {
+            EntityManager em = Conector.getEntityManager();
+            em.getTransaction().begin();
+            String s_id = modAlumView.getSearchBar().getText();
+            Alumno alumAux = em.find(Alumno.class, s_id);
+            modAlumView.getDni().setText(alumAux.getDni());
+            modAlumView.getApellidoPaterno().setText(alumAux.getApellidoPaterno());
+            modAlumView.getApellidoMaterno().setText(alumAux.getApellidoMaterno());
+            modAlumView.getNombres().setText(alumAux.getNombres());
+            modAlumView.getFechaNac().setText(alumAux.getFechaNacimiento().toString());
+            modAlumView.getSexo().setText(alumAux.getSexo());
+            modAlumView.getNuss().setText(alumAux.getNuss());
+            modAlumView.getEmail().setText(alumAux.getEmail());
+            modAlumView.getTelefono().setText(String.valueOf(alumAux.getTlf()));
+            modAlumView.setPhoto(new ImageIcon("src/main/resources/img/photo-default.jpg"));
+            em.getTransaction().commit();
+            em.close();
+            JOptionPane.showMessageDialog(modAlumView, "Datos recuperados correctamente", "¡Éxito!", JOptionPane.PLAIN_MESSAGE);
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(modAlumView, "Fallo al recuperar los datos", "Algo salió mal", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}

03810b114a478eb6d9a8a63e7a0f495587077e75
alejandrodaniel <alesaho05@gmail.com>
2025-05-05 21:01:06 +0200

    Implements action listener

--- src/main/java/dev/alejandrosahonero/logic/options/manage/profesor/ModProButtonController.java
+++ src/main/java/dev/alejandrosahonero/logic/options/manage/profesor/ModProButtonController.java
@@ -1,4 +1,14 @@
 package dev.alejandrosahonero.logic.options.manage.profesor;
 
-public class ModProButtonController {
+import dev.alejandrosahonero.gui.options.manage.ModProView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ModProButtonController implements ActionListener {
+    ModProView modProView;
+    public ModProButtonController(ModProView modProView) {
+        this.modProView = modProView;
+    }
+    public void actionPerformed(ActionEvent e) {}
 }

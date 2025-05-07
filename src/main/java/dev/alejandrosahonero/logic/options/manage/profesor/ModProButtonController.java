dada35a7284087d71a8ae5c26ba1d5c8553d4d15
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-05-07 19:55:37 +0200

    Test view

--- src/main/java/dev/alejandrosahonero/logic/options/manage/profesor/ModProButtonController.java
+++ src/main/java/dev/alejandrosahonero/logic/options/manage/profesor/ModProButtonController.java
@@ -1,9 +1,15 @@
 package dev.alejandrosahonero.logic.options.manage.profesor;
 
+import dev.alejandrosahonero.db.Conector;
+import dev.alejandrosahonero.db.Modulo;
 import dev.alejandrosahonero.gui.options.manage.ModProView;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModProButtonController implements ActionListener {
     ModProView modProView;
@@ -11,4 +17,13 @@ public class ModProButtonController implements ActionListener {
         this.modProView = modProView;
     }
     public void actionPerformed(ActionEvent e) {}
+    public static List<String> getAllModulos(){
+        EntityManager em = Conector.getEntityManager();
+        em.getTransaction().begin();
+        Query query = em.createQuery("SELECT m.siglas FROM Modulo m");
+        List<String> allModulos = query.getResultList();
+        em.getTransaction().commit();
+        em.close();
+        return allModulos;
+    }
 }

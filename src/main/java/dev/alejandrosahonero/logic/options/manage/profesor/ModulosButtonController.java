17890cfcf2fe8c4fb3b133e80d5ecf63efa26ea7
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-05-08 18:09:27 +0200

    Add constructor for mod view too

--- src/main/java/dev/alejandrosahonero/logic/options/manage/profesor/ModulosButtonController.java
+++ src/main/java/dev/alejandrosahonero/logic/options/manage/profesor/ModulosButtonController.java
@@ -1,6 +1,7 @@
 package dev.alejandrosahonero.logic.options.manage.profesor;
 
 import dev.alejandrosahonero.gui.options.manage.AddModProView;
+import dev.alejandrosahonero.gui.options.manage.ModProView;
 import dev.alejandrosahonero.gui.options.manage.NewProView;
 
 import java.awt.event.ActionEvent;
@@ -8,8 +9,12 @@ import java.awt.event.ActionListener;
 
 public class ModulosButtonController implements ActionListener {
     private NewProView newProView;
+    private ModProView modProView;
     public ModulosButtonController(NewProView newProView) {
         this.newProView = newProView;
+    }
+    public ModulosButtonController(ModProView modProView) {
+        this.modProView = modProView;
     }
     public void actionPerformed(ActionEvent e) {
         new AddModProView().setVisible(true);

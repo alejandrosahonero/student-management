9241a64f966a30e71c10f9cece2498016629edad
alejandrodaniel <alesaho05@gmail.com>
2025-05-08 14:05:06 +0200

    Add a single controller for modulosButton on NewProView

---
+++ src/main/java/dev/alejandrosahonero/logic/options/manage/profesor/ModulosButtonController.java
@@ -0,0 +1,17 @@
+package dev.alejandrosahonero.logic.options.manage.profesor;
+
+import dev.alejandrosahonero.gui.options.manage.AddModProView;
+import dev.alejandrosahonero.gui.options.manage.NewProView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ModulosButtonController implements ActionListener {
+    private NewProView newProView;
+    public ModulosButtonController(NewProView newProView) {
+        this.newProView = newProView;
+    }
+    public void actionPerformed(ActionEvent e) {
+        new AddModProView().setVisible(true);
+    }
+}

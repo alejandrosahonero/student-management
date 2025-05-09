759a6fd5f36b24aeede21b31a0581a6caa49e52d
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-05-09 21:01:15 +0200

    Add reset JTextFields

--- src/main/java/dev/alejandrosahonero/logic/options/manage/asignatura/NewAsigButtonController.java
+++ src/main/java/dev/alejandrosahonero/logic/options/manage/asignatura/NewAsigButtonController.java
@@ -2,6 +2,7 @@ package dev.alejandrosahonero.logic.options.manage.asignatura;
 
 import dev.alejandrosahonero.db.Asignatura;
 import dev.alejandrosahonero.db.Conector;
+import dev.alejandrosahonero.gui.MainView;
 import dev.alejandrosahonero.gui.options.manage.asignatura.NewAsigView;
 
 import javax.persistence.EntityManager;
@@ -32,6 +33,15 @@ public class NewAsigButtonController implements ActionListener {
             em.close();
 
             JOptionPane.showMessageDialog(newAsigView, "Se añadió la asignatura: "+n_nombre.toUpperCase()+" correctamente", "¡Exito!", JOptionPane.PLAIN_MESSAGE);
+
+            //Reset fields
+            newAsigView.getSiglas().setText("SIGLAS");
+            newAsigView.getSiglas().setForeground(MainView.getTextFieldColor());
+            newAsigView.getNombre().setText("Nombre");
+            newAsigView.getNombre().setForeground(MainView.getTextFieldColor());
+            newAsigView.getDescripcion().setText("");
+            newAsigView.getHoras().setText("Horas");
+            newAsigView.getHoras().setForeground(MainView.getTextFieldColor());
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(newAsigView, "Error al cargar datos", "Algo salio mal", JOptionPane.ERROR_MESSAGE);
         }

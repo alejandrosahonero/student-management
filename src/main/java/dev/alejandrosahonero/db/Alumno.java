b0150348909ddc880c0929ecf3d2652ab4d4fbf2
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-05-09 21:32:54 +0200

    Remove useless "asignaturas" List attribute on Alumno

--- src/main/java/dev/alejandrosahonero/db/Alumno.java
+++ src/main/java/dev/alejandrosahonero/db/Alumno.java
@@ -1,32 +1,21 @@
 package dev.alejandrosahonero.db;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.sql.Date;
 
 @Entity
-public class Alumno extends Persona implements Serializable {;
+public class Alumno extends Persona implements Serializable {
     @ManyToOne
     private Modulo modulo;
     private String nuss;
-    private ArrayList<Asignatura> asignaturas = new ArrayList<>();
 
     public Alumno() {}
     public Alumno(String dni, String apellidoPaterno, String apellidoMaterno, String nombre, Date fechaNacimiento, String sexo, String email, int tlf, Modulo modulo, String nuss) {
         super(dni, apellidoPaterno, apellidoMaterno, nombre, fechaNacimiento, sexo, email, tlf);
         this.modulo = modulo;
         this.nuss = nuss;
-        this.asignaturas = new ArrayList<>();
-    }
-    public Alumno(String dni, String apellidoPaterno, String apellidoMaterno, String nombre, Date fechaNacimiento, String sexo, String email, int tlf, Modulo modulo, String nuss, ArrayList<Asignatura> asignaturas) {
-        super(dni, apellidoPaterno, apellidoMaterno, nombre, fechaNacimiento, sexo, email, tlf);
-        this.modulo = modulo;
-        this.nuss = nuss;
-        this.asignaturas = asignaturas;
     }
 
     //Getters
@@ -41,8 +30,5 @@ public class Alumno extends Persona implements Serializable {;
     }
     public void setNuss(String nuss) {
         this.nuss = nuss;
-    }
-    public ArrayList<Asignatura> getAsignaturas() {
-        return asignaturas;
     }
 }

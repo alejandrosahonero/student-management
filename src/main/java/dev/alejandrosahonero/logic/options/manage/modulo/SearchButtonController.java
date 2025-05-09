9b56071400d600f04e44e4681ce8a29289450c45
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-05-09 19:21:19 +0200

    Add search functionality

--- src/main/java/dev/alejandrosahonero/logic/options/manage/modulo/SearchButtonController.java
+++ src/main/java/dev/alejandrosahonero/logic/options/manage/modulo/SearchButtonController.java
@@ -1,7 +1,12 @@
 package dev.alejandrosahonero.logic.options.manage.modulo;
 
+import dev.alejandrosahonero.db.Conector;
+import dev.alejandrosahonero.db.Modulo;
 import dev.alejandrosahonero.gui.options.manage.modulo.ModModView;
 
+import javax.persistence.EntityManager;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,5 +15,27 @@ public class SearchButtonController implements ActionListener {
     public SearchButtonController(ModModView modModView) {
         this.modModView = modModView;
     }
-    public void actionPerformed(ActionEvent e) {}
+    public void actionPerformed(ActionEvent e) {
+        EntityManager em = Conector.getEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            String s_siglas = modModView.getSearchBar().getText();
+            Modulo mAux = em.find(Modulo.class, s_siglas);
+
+            modModView.getSiglas().setText(mAux.getSiglas());
+            modModView.getSiglas().setForeground(Color.BLACK);
+            modModView.getNombre().setText(mAux.getNombre());
+            modModView.getNombre().setForeground(Color.BLACK);
+            modModView.getDescripcion().setText(mAux.getDescripcion());
+
+            em.getTransaction().commit();
+            em.close();
+            JOptionPane.showMessageDialog(modModView, "Datos recuperados correctamente", "¡Éxito!", JOptionPane.PLAIN_MESSAGE);
+
+        } catch (Exception ex) {
+            em.close();
+            JOptionPane.showMessageDialog(modModView, "Fallo al recuperar los datos", "Algo salió mal", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }

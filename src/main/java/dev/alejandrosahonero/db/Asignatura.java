5dcac0347f2032738916cd6bda71e0b9dc864713
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-05-09 20:18:47 +0200

    Add "horas" new var attribute

--- src/main/java/dev/alejandrosahonero/db/Asignatura.java
+++ src/main/java/dev/alejandrosahonero/db/Asignatura.java
@@ -11,12 +11,14 @@ public class Asignatura {
     private String siglas;
     private String nombre;
     private String descripcion;
+    private int horas;
 
     public Asignatura() {}
-    public Asignatura(String siglas, String nombre, String descripcion) {
+    public Asignatura(String siglas, String nombre, String descripcion, int horas) {
         this.siglas = siglas;
         this.nombre = nombre;
         this.descripcion = descripcion;
+        this.horas = horas;
     }
 
     @Override
@@ -34,4 +36,5 @@ public class Asignatura {
     public String getDescripcion() {
         return descripcion;
     }
+    public int getHoras() {return horas;}
 }

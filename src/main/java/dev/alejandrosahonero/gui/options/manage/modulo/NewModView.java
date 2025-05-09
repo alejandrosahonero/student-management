c80536ed0af14e9bd4f1e3d6bd38da2b37497d4f
adsahoneoa <adsahoneroa01@larioja.edu.es>
2025-05-09 18:19:44 +0200

    Improve buttons design

--- src/main/java/dev/alejandrosahonero/gui/options/manage/modulo/NewModView.java
+++ src/main/java/dev/alejandrosahonero/gui/options/manage/modulo/NewModView.java
@@ -6,12 +6,16 @@ import dev.alejandrosahonero.logic.options.manage.modulo.AsignaturasButtonContro
 import dev.alejandrosahonero.logic.options.manage.modulo.NewModButtonController;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class NewModView extends MainManageView {
-    final int margin = 50;
+    final int marginTitle = 60;
+    final int marginHori = 132;
+    final int marginVert = 72;
 
     private JLabel background, title1, siglas_l, nombre_l, descripcion_l, asignaturas_l;
-    private JTextField siglas, nombre, descripcion;
+    private JTextField siglas, nombre;
+    private JTextArea descripcion;
     private JButton asignaturasButton, doneButton;
 
     public NewModView(){
@@ -33,45 +37,49 @@ public class NewModView extends MainManageView {
     @Override
     protected void titles(){
         title1 = createTitle("INFORMACIÓN DEL MÓDULO", 50, 0x3D5A40);
-        title1.setBounds(margin, margin, getButtonsWidth()*3, getButtonsHeight());
+        title1.setBounds(marginHori/2, marginTitle, getButtonsWidth()*3, getButtonsHeight());
         background.add(title1);
     }
     @Override
     protected void subTitles(){
         siglas_l = createTitle("SIGLAS", 20, 0x3D5A40);
-        siglas_l.setBounds(margin, margin*2+getButtonsHeight(), getButtonsWidth(), getButtonsHeight());
+        siglas_l.setBounds(marginHori, marginTitle*2+getButtonsHeight()/3, getButtonsWidth(), getButtonsHeight());
         background.add(siglas_l);
         nombre_l = createTitle("NOMBRE", 20, 0x3D5A40);
-        nombre_l.setBounds(margin*2+getButtonsWidth(), margin*2+getButtonsHeight(), getButtonsWidth(), getButtonsHeight());
+        nombre_l.setBounds(marginHori, marginTitle*2+marginVert+getButtonsHeight()/3*4, getButtonsWidth(), getButtonsHeight());
         background.add(nombre_l);
-        descripcion_l = createTitle("DESCRIPCIÓN", 20, 0x3D5A40);
-        descripcion_l.setBounds(margin*3+getButtonsWidth()*2, margin*2+getButtonsHeight(), getButtonsWidth(), getButtonsHeight());
-        background.add(descripcion_l);
         asignaturas_l = createTitle("ASIGNATURAS", 20, 0x3D5A40);
-        asignaturas_l.setBounds(margin*4+getButtonsWidth()*3, margin*2+getButtonsHeight(), getButtonsWidth(), getButtonsHeight());
+        asignaturas_l.setBounds(marginHori, marginTitle*2+marginVert*2+getButtonsHeight()/3*7, getButtonsWidth(), getButtonsHeight());
         background.add(asignaturas_l);
+        descripcion_l = createTitle("DESCRIPCIÓN", 20, 0x3D5A40);
+        descripcion_l.setBounds(marginHori*2+getButtonsWidth(), marginTitle*2+getButtonsHeight()/3, getButtonsWidth(), getButtonsHeight());
+        background.add(descripcion_l);
     }
     private void data(){
         siglas = createTextField("SIGLAS", 15);
-        siglas.setBounds(margin, margin*3+getButtonsHeight()*2, getButtonsWidth(), getButtonsHeight());
+        siglas.setBounds(marginHori, marginTitle*2+getButtonsHeight(), getButtonsWidth(), getButtonsHeight());
         background.add(siglas);
 
         nombre = createTextField("Nombre", 15);
-        nombre.setBounds(margin*2+getButtonsWidth(), margin*3+getButtonsHeight()*2, getButtonsWidth(), getButtonsHeight());
+        nombre.setBounds(marginHori, marginTitle*2+marginVert+getButtonsHeight()*2, getButtonsWidth(), getButtonsHeight());
         background.add(nombre);
 
-        descripcion = createTextField("Descripción", 15);
-        descripcion.setBounds(margin*3+getButtonsWidth()*2, margin*3+getButtonsHeight()*2, getButtonsWidth(), getButtonsHeight());
-        background.add(descripcion);
-
-        asignaturasButton = createButton("Asignar Asignaturas", 15, 0x3D5A40, 0xffffff);
-        asignaturasButton.setBounds(margin*4+getButtonsWidth()*3, margin*3+getButtonsHeight()*2, getButtonsWidth(), getButtonsHeight());
+        asignaturasButton = createButton("Asignar Asignaturas", 20, 0x3D5A40, 0xffffff);
+        asignaturasButton.setBounds(marginHori, marginTitle*2+marginVert*2+getButtonsHeight()*3, getButtonsWidth(), getButtonsHeight());
         asignaturasButton.addActionListener(new AsignaturasButtonController(this));
         background.add(asignaturasButton);
+
+        descripcion = new JTextArea();
+        descripcion.setFont(new Font("Tahoma", Font.BOLD, 15));
+        descripcion.setLineWrap(true);
+        descripcion.setWrapStyleWord(true);
+        descripcion.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+        descripcion.setBounds(marginHori*2+getButtonsWidth(), marginTitle*2+getButtonsHeight(), getButtonsWidth(), getButtonsHeight()*3+marginVert*2);
+        background.add(descripcion);
     }
     private void doneButton(){
         doneButton = createButton("AÑADIR", 25, 0x3D5A40, 0xffffff);
-        doneButton.setBounds(margin*9/2+getButtonsWidth()*3, margin*5/2+getButtonsHeight()*3, getButtonsWidth(), getButtonsHeight());
+        doneButton.setBounds(marginHori*3+getButtonsWidth()*2, marginTitle*2+getButtonsHeight()+marginVert*2, getButtonsWidth(), getButtonsHeight());
         doneButton.addActionListener(new NewModButtonController(this));
         background.add(doneButton);
     }
